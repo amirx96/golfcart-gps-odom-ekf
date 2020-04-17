aeef10071c73c03abea36a5346256896c3fce756
Jacob <JacobHartzer@gmail.com>
2020-04-17 11:39:35 -0500

    Stable python multi-update EKF

--- python/golfcartEKF.py
+++ python/golfcartEKF.py
@@ -8,21 +8,20 @@ np.core.arrayprint._line_width = 160
 
 import message_filters
 from math import pi
-from geometry_msgs.msg import Point, Pose2D, Quaternion, Vector3
+from geometry_msgs.msg import Point, Pose2D, Quaternion, Vector3, PoseStamped
 from vn300.msg import gps, sensors
 from pacmod_msgs.msg import VehicleSpeedRpt, SystemRptFloat
 import tf
-from nav_msgs.msg import Odometry
+from nav_msgs.msg import Odometry, Path
 
-# (24 -45)
-# (79 -2)
 class GolfCartEKF:
 
     def __init__(self):
 
         rospy.init_node('GolfCartEKF')
         # Parameters
-        self.wheelbase = 3.4
+        self.L = 3.4
+        self.SR = 22
 
         # create subscribers
         self.vn300_imu_sub      = rospy.Subscriber('/vectornav/imu',    sensors,    self.predict_step_imu)
@@ -34,8 +33,9 @@ class GolfCartEKF:
         ts = message_filters.ApproximateTimeSynchronizer([self.pacmod_speed_sub, self.pacmod_steer_sub], 10, 0.03)
         ts.registerCallback(self.update_step_pacmod)
 
-
         self.state_pub = rospy.Publisher('golfcartEKF/state', Odometry, queue_size=10)
+        self.path_pub  = rospy.Publisher('golfcartEKF/path',  Path,     queue_size=10, latch=True)
+
         self.state_odom = Odometry()
         self.state_odom.header.frame_id = 'golfcart_frame'
         self.state_odom.child_frame_id = 'world'
@@ -54,7 +54,7 @@ class GolfCartEKF:
         self.lat_home = 0.0
         self.lon_lat_init = False
 
-        # Index - State Variable - Initial Value:
+        # Index - Variable- Initial Value:
         #     0 - x       - 0
         #     1 - y       - 0
         #     2 - theta   - 0
@@ -62,75 +62,30 @@ class GolfCartEKF:
         #     4 - vy      - 0
         #     5 - omega   - 0
 
-        #     6 - IMU_x   - 2.5
-        #     7 - IMU_y   - 0.5
 
-        #     8 - SR      - 22
+        self.state = np.zeros([6,1])
+        self.state[2] = -pi/2
 
-        #     9  - Maxy   - 0
-        #     10 - Maxz   - 0
-        #     11 - Mayx   - 0
-        #     12 - Mayz   - 0
-        #     13 - Mazx   - 0
-        #     14 - Mazy   - 0
-        #     15 - Sax    - 1
-        #     16 - Say    - 1
-        #     17 - Saz    - 1
-        #     18 - Bax    - 0
-        #     19 - Bay    - 0
-        #     20 - Baz    - 0
-
-        #     21 - Mgxy   - 0
-        #     22 - Mgxz   - 0
-        #     23 - Mgyx   - 0
-        #     24 - Mgyz   - 0
-        #     25 - Mgzx   - 0
-        #     26 - Mgzy   - 0
-        #     27 - Sgx    - 1
-        #     28 - Sgy    - 1
-        #     29 - Sgz    - 1
-        #     30 - Bgx    - 0
-        #     31 - Bgy    - 0
-        #     32 - Bgz    - 0
-
-        self.state = np.zeros([33,1])
-        self.state[5]  = 0.001
-        self.state[6]  = 2.5
-        self.state[7]  = 0.5
-        self.state[8]  = 22.0
-        self.state[9]  = 1.0
-        self.state[16] = 1.0
-        self.state[17] = 1.0
-        self.state[18] = 1.0
-        self.state[28] = 1.0
-        self.state[29] = 1.0
-        self.state[30] = 1.0
-        self.state_pacmod = self.state
-
-        self.Q = np.zeros([33,33])
-        np.fill_diagonal( self.Q, \
-            np.concatenate(( \
-                np.repeat(10,3), \
-                np.repeat(10,3), \
-                np.repeat(1,27))))
+        self.Q = np.zeros([6,6])
+        np.fill_diagonal( self.Q, [0.1, 0.1, 0.1, 0.01, 0.01, 0.01] )
 
         self.state_cov = self.Q
-
-        self.R_gps = np.identity(3) * 2.0
         
-        self.R_pcm = np.zeros([7,7])
-        np.fill_diagonal( self.R_pcm, 100 )
+        self.R_pcm = np.zeros([3,3])
+        np.fill_diagonal( self.R_pcm, [0.05, 0.07, 0.01] )
 
+        self.R_gps = np.zeros([3,3])
+        np.fill_diagonal( self.R_gps, [0.4, 0.4, 0.75] )
 
     def predict_step_imu(self, msg):
         if self.accel_time_init:
             dt = msg.header.stamp.to_sec() - self.accel_time
-            F = np.identity(33)
+            F = np.identity(6)
             F[0,3] = dt 
             F[1,4] = dt 
             F[5,5] = 0.0
 
-            B = np.zeros([33,3])
+            B = np.zeros([6,3])
             B[0,0] =  (dt**2)/2*math.cos(self.state[2])
             B[0,1] = -(dt**2)/2*math.sin(self.state[2])
             B[1,0] =  (dt**2)/2*math.sin(self.state[2])
@@ -142,8 +97,8 @@ class GolfCartEKF:
             B[4,1] =  dt*math.cos(self.state[2])
             B[5,2] =  1.0
 
-            U = np.array([[msg.Accel.x],[msg.Accel.y],[msg.Gyro.z]])
-            
+            U = np.array([[msg.Accel.x],[msg.Accel.y],[-msg.Gyro.z]])
+
             self.state = np.matmul(F, self.state) + np.matmul(B, U)
             
             # P_k|k-1 = F_k * P_k-1|k-1 * F_k^T + Q_k
@@ -158,80 +113,37 @@ class GolfCartEKF:
     def update_step_pacmod(self, speed_msg, steer_msg):
         
         if self.pacmod_time_init:
-            # print('\nPacmod')
-            dt = speed_msg.header.stamp.to_sec() - self.pacmod_time
-            F = np.identity(7)
-            F[0,3] = dt 
-            F[1,4] = dt 
-            F[2,5] = dt
 
-            X = np.array( [ self.state_pacmod[0], \
-                            self.state_pacmod[1], \
-                            self.state_pacmod[2], \
-                          [ speed_msg.vehicle_speed*math.cos(self.state_pacmod[2])], \
-                          [ speed_msg.vehicle_speed*math.sin(self.state_pacmod[2])], \
-                          [ speed_msg.vehicle_speed*math.tan(-steer_msg.output/self.state[8])/self.wheelbase], \
-                            self.state[8]])
+            z = np.array([  [speed_msg.vehicle_speed * math.cos(self.state[2])] , \
+                            [speed_msg.vehicle_speed * math.sin(self.state[2])] , \
+                            [speed_msg.vehicle_speed / self.L * math.tan(steer_msg.output / self.SR) ] ])
 
-            v = math.sqrt(self.state[3]**2 + self.state[4]**2)
-
-            # print(v)
-            # print(speed_msg.vehicle_speed)
-            self.H_pcm = np.zeros([7,33])
-            self.H_pcm[0,0] = 1.0
-            self.H_pcm[1,1] = 1.0
-            self.H_pcm[2,2] = 1.0
-            self.H_pcm[3,3] = 1.0
-            self.H_pcm[4,4] = 1.0
-            self.H_pcm[5,5] = 1.0
-            self.H_pcm[6,8] = 1.0 
-
-            
-            if not v == 0:
-                self.H_pcm[0,3] =  dt * math.cos(self.state_pacmod[2]) / v * self.state[3]
-                self.H_pcm[0,4] =  dt * math.cos(self.state_pacmod[2]) / v * self.state[4]
-                self.H_pcm[0,3] =  dt * math.sin(self.state_pacmod[2]) / v * self.state[3]
-                self.H_pcm[0,4] =  dt * math.sin(self.state_pacmod[2]) / v * self.state[4]
-                self.H_pcm[3,3] =       math.cos(self.state_pacmod[2]) / v * self.state[3]
-                self.H_pcm[3,4] =       math.cos(self.state_pacmod[2]) / v * self.state[4]
-                self.H_pcm[4,3] =       math.sin(self.state_pacmod[2]) / v * self.state[3]
-                self.H_pcm[4,4] =       math.sin(self.state_pacmod[2]) / v * self.state[4]
-
-
+            self.H_pcm = np.zeros([3,6])
+            self.H_pcm[0,3] = 1.0
+            self.H_pcm[1,4] = 1.0
+            self.H_pcm[2,5] = 1.0
 
             # y_k       = z_k - h(xhat_k|k-1)
-            self.y_pcm = np.matmul( F, X ) - self.state[np.array([0,1,2,3,4,5,8])]
-            print('\n')
-            print(self.state[8])
-            print(- steer_msg.output / math.atan2( self.wheelbase * self.state[5], speed_msg.vehicle_speed ))
-            print(self.state[8] + steer_msg.output / math.atan2( self.wheelbase * self.state[5], speed_msg.vehicle_speed ))
-            
-            if abs(steer_msg.output) > 2.0 and not speed_msg.vehicle_speed == 0.0 and not self.state[5] == 0:
-                self.H_pcm[6,3] = -steer_msg.output * self.wheelbase * self.state[5] * self.state[3] / ( math.atan2(self.wheelbase * self.state[5], v) * (self.wheelbase**2 * self.state[5]**2 + v**2) * v )
-                self.H_pcm[6,4] = -steer_msg.output * self.wheelbase * self.state[5] * self.state[4] / ( math.atan2(self.wheelbase * self.state[5], v) * (self.wheelbase**2 * self.state[5]**2 + v**2) * v )
-                self.H_pcm[6,5] =  steer_msg.output * self.wheelbase * self.state[5] *       v       / ( math.atan2(self.wheelbase * self.state[5], v) * (self.wheelbase**2 * self.state[5]**2 + v**2)     )
-                self.y_pcm[6]   = -self.state[8] - steer_msg.output / math.atan2( self.wheelbase * self.state[5], speed_msg.vehicle_speed )
-
+            self.y_pcm = z - self.state[3:6]
+            # print(self.y_pcm.T)
 
             # # S_k       = H_k * P_k|k-1 * H_k^T + R_kError
             self.S_pcm = np.matmul(self.H_pcm, np.matmul(self.state_cov, self.H_pcm.T)) + self.R_pcm 
-
+            # print(self.S_pcm)
 
             # # K_k       = P_k|k-1 * H_k^T * S_k^-1
             self.K_pcm = np.matmul(self.state_cov, np.matmul(self.H_pcm.T, np.linalg.inv(self.S_pcm)))
-
+            # print(self.K_pcm)
 
             # # xhat_k|k  = xhat_k|k-1 + K_k * y_k
             self.state = self.state + np.matmul(self.K_pcm, self.y_pcm)
 
             # # P_k|k     = (I - K_k * H_k) * P_k|k-1
-            self.state_cov = np.matmul((np.eye(33) - np.matmul(self.K_pcm, self.H_pcm)), self.state_cov)
-            
+            self.state_cov = np.matmul((np.eye(6) - np.matmul(self.K_pcm, self.H_pcm)), self.state_cov)
 
         else: 
             self.pacmod_time_init = True
 
-        self.state_pacmod = self.state
         self.pacmod_time = speed_msg.header.stamp.to_sec()
         
 
@@ -262,15 +174,15 @@ class GolfCartEKF:
             self.dN_prev = dN
 
             # # Measurement in matrix format
-            measurement = np.array([[dE],[dN],[dT]])
+            z = np.array([[dE],[dN],[dT]])
 
-            self.H_gps = np.zeros([3,33])
+            self.H_gps = np.zeros([3,6])
             self.H_gps[0,0] = 1 
             self.H_gps[1,1] = 1 
             self.H_gps[2,2] = 1
 
             # # y_k       = z_k - h(xhat_k|k-1)
-            self.y_gps = measurement - np.matmul(self.H_gps, self.state)
+            self.y_gps = z - np.matmul(self.H_gps, self.state)
 
             # # S_k       = H_k * P_k|k-1 * H_k^T + R_kError
             self.S_gps = np.matmul(self.H_gps, np.matmul(self.state_cov, self.H_gps.T)) + self.R_gps
@@ -283,7 +195,7 @@ class GolfCartEKF:
             
 
             # # P_k|k     = (I - K_k * H_k) * P_k|k-1
-            self.state_cov = np.matmul((np.eye(33) - np.matmul(self.K_gps, self.H_gps)), self.state_cov)
+            self.state_cov = np.matmul((np.eye(6) - np.matmul(self.K_gps, self.H_gps)), self.state_cov)
 
             self.last_gps_time = msg.UtcTime.millisecond  
 
@@ -296,10 +208,9 @@ class GolfCartEKF:
 
         rate = rospy.Rate(50) # 10 Hz
         while(not rospy.is_shutdown()):
-
             self.state_odom.header.stamp = rospy.Time.now()
             self.state_odom.pose.pose.position = Point(self.state[0],self.state[1],0)
-            self.state_odom.pose.pose.orientation = Quaternion(0,0,math.sin(self.state[2]/2),math.cos(self.state[2]/2))
+            self.state_odom.pose.pose.orientation = Quaternion(0, 0, math.sin(self.state[2]/2), math.cos(self.state[2]/2))
             self.state_odom.pose.covariance = self.state_cov[0:6,0:6].flatten()
             
             self.state_odom.twist.twist.linear = Vector3(self.state[3],self.state[4],0)
@@ -308,7 +219,7 @@ class GolfCartEKF:
 
             self.state_pub.publish( self.state_odom )
 
-            self.br.sendTransform(( self.state[0], self.state[1], 0),
+            self.br.sendTransform(( 0, 0, 0),
                                     tf.transformations.quaternion_from_euler(0, 0, 0),
                                     rospy.Time.now(),
                                     self.state_odom.header.frame_id,
@@ -316,7 +227,23 @@ class GolfCartEKF:
 
             rate.sleep()
 
+class OdomToPath:
+    def __init__(self):
+        self.path_pub = rospy.Publisher('/golfcartEKF/path', Path, latch=True, queue_size=10)
+        self.odom_sub = rospy.Subscriber('/golfcartEKF/state', Odometry, self.odom_cb, queue_size=10)
+        self.path = Path()
+
+    def odom_cb(self, msg):
+        cur_pose = PoseStamped()
+        cur_pose.header = msg.header
+        cur_pose.pose = msg.pose.pose
+        self.path.header = msg.header
+        self.path.poses.append(cur_pose)
+        self.path_pub.publish(self.path)
+
 
 if __name__ == '__main__':
     GolfCart_EKF = GolfCartEKF()
+    odom_to_path = OdomToPath()
     GolfCart_EKF.run()
+    
