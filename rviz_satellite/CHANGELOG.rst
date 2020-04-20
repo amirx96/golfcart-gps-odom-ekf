f1f5a443a5aa5bd02f39b757a79e0ff708c43f68
amirx96 <amir.darwesh@gmail.com>
2020-04-20 02:28:05 +0000

    add sattelite imagery

---
+++ rviz_satellite/CHANGELOG.rst
@@ -0,0 +1,38 @@
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+Changelog for package rviz_satellite
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+Forthcoming
+-----------
+* Remove NED and NWU frame conversion options
+
+2.0.0 (2020-04-17)
+------------------
+* Drop Qt4 support
+
+1.3.0 (2020-04-17)
+------------------
+* Fix setting the fixed frame
+* Fix race when reading the blocks property
+* Set tile fetching preference to cache
+* Remove the robot frame property. Instead use the frame from the NavSatFix topic
+* Fix demo.launch
+* Fix uninitialized variable
+
+1.2.0 (2019-03-07)
+------------------
+* Rewrite of the tile loader
+* Display an error if the tile server is invalid
+* Move map towards the robot's z-position
+* Use XDG cache path for caching tiles
+* Incorporate orientation of tile frame when rendering map
+
+1.1.0 (2018-12-04)
+------------------
+* Integrate package version via CMake
+* Detect Qt 5 automatically, adding support for ROS Lunar, Melodic, et al
+* Remove unused opencv linking
+
+1.0.0 (2018-11-08)
+------------------
+* Initial release
---
+++ rviz_satellite/src/detail/TileDownloader.h
@@ -0,0 +1,125 @@
+/* Copyright 2018-2019 TomTom N.V.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
+
+#include <functional>
+
+#include <QDir>
+#include <QtCore>
+#include <QtNetwork>
+#include <QImage>
+#include <QImageReader>
+#include <QStandardPaths>
+#include <QString>
+#include <QCryptographicHash>
+
+#include <ros/ros.h>
+
+#include "detail/ErrorRateManager.h"
+#include "TileId.h"
+
+namespace detail
+{
+/**
+ * @brief Tile downloader
+ *
+ * This class encapsulates away all the Qt stuff regarding downloading.
+ */
+class TileDownloader : public QObject
+{
+  Q_OBJECT
+  QNetworkAccessManager* manager;
+  std::function<void(TileId, QImage)> callback;
+
+public:
+  detail::ErrorRateManager<std::string> errorRates;
+
+  TileDownloader(decltype(callback) callback) : manager(new QNetworkAccessManager(this)), callback(std::move(callback))
+  {
+    connect(manager, SIGNAL(finished(QNetworkReply*)), SLOT(downloadFinished(QNetworkReply*)));
+
+    QNetworkDiskCache* diskCache = new QNetworkDiskCache(this);
+    QString const cachePath =
+        QDir(QStandardPaths::writableLocation(QStandardPaths::GenericCacheLocation)).filePath("rviz_satellite");
+    diskCache->setCacheDirectory(cachePath);
+    // there is no option to disable maximum cache size
+    diskCache->setMaximumCacheSize(std::numeric_limits<qint64>::max());
+    manager->setCache(diskCache);
+  }
+
+  /**
+   * @brief Load a specific tile
+   *
+   * Since QNetworkDiskCache is used, tiles will be loaded from the file system if they have been cached. Otherwise they
+   * get downloaded.
+   */
+  void loadTile(TileId const& tileId)
+  {
+    // see https://foundation.wikimedia.org/wiki/Maps_Terms_of_Use#Using_maps_in_third-party_services
+    auto const requestUrl = QUrl(QString::fromStdString(tileURL(tileId)));
+    ROS_DEBUG_STREAM_NAMED("rviz_satellite", "Loading tile " << requestUrl.toString().toStdString());
+
+    QNetworkRequest request(requestUrl);
+    char constexpr agent[] = "rviz_satellite/" RVIZ_SATELLITE_VERSION " (+https://github.com/gareth-cross/"
+                             "rviz_satellite)";
+    request.setHeader(QNetworkRequest::KnownHeaders::UserAgentHeader, agent);
+    QVariant variant;
+    variant.setValue(tileId);
+    request.setAttribute(QNetworkRequest::CacheLoadControlAttribute, QNetworkRequest::CacheLoadControl::PreferCache);
+    request.setAttribute(QNetworkRequest::User, variant);
+    manager->get(request);
+  }
+
+public slots:
+  void downloadFinished(QNetworkReply* reply)
+  {
+    QVariant const variant = reply->request().attribute(QNetworkRequest::User);
+    TileId const tileId = variant.value<TileId>();
+
+    QUrl const url = reply->url();
+    if (reply->error())
+    {
+      ROS_ERROR_STREAM("Got error when loading tile: " << reply->errorString().toStdString());
+      errorRates.issueError(tileId.tileServer);
+      return;
+    }
+    else
+    {
+      errorRates.issueSuccess(tileId.tileServer);
+    }
+
+    // log if tile comes from cache or web
+    bool const fromCache = reply->attribute(QNetworkRequest::SourceIsFromCacheAttribute).toBool();
+    if (fromCache)
+    {
+      ROS_DEBUG_STREAM_NAMED("rviz_satellite", "Loaded tile from cache " << url.toString().toStdString());
+    }
+    else
+    {
+      ROS_DEBUG_STREAM_NAMED("rviz_satellite", "Loaded tile from web " << url.toString().toStdString());
+    }
+
+    QImageReader reader(reply);
+    if (!reader.canRead())
+    {
+      ROS_ERROR_STREAM("Unable to decode image at " << reply->request().url().toString().toLatin1().data());
+      return;
+    }
+
+    callback(tileId, reader.read());
+
+    reply->deleteLater();
+  }
+};
+
+}  // namespace detail
