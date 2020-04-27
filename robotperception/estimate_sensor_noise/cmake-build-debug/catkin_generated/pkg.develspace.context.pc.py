4e3f9fba994c6a880c44869d03d5c630414db4f0
amirx96 <amir.darwesh@gmail.com>
2020-04-27 16:06:18 -0500

    remove robotic perception

--- robotperception/estimate_sensor_noise/cmake-build-debug/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "estimate_sensor_noise"
-PROJECT_SPACE_DIR = "/home/usl/catkin_ws/src/robotperception/estimate_sensor_noise/cmake-build-debug/devel"
-PROJECT_VERSION = "0.0.0"
